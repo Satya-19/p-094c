703c413657abac6b3ea1c1f636eb522be1088af4
Satyajeet <satyajeet.malla2@gmail.com>
2020-03-02 17:04:31 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/gallery', (req, res) => {
+res.render('gallery');
+});
+
 module.exports = router;
