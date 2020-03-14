79bd1a37a7a9280625e07c526c1881909671bc6d
Satyajeet <satyajeet.malla2@gmail.com>
2020-03-14 17:59:38 +0530

    add pages

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET gallery page. */
 router.get('/gallery', (req, res) => {
   res.render('gallery');
 });
 
+/* GET register page. */
 router.get('/register', (req, res) => {
   res.render('register');
-})
+});
+
+/* GET contact us page. */
+router.get('/contact', (req, res) => {
+  res.render('contact');
+});
 
 module.exports = router;
